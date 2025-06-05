c117ff0321435fd5bf99755e2c4758a6e33bde96
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2025-06-05 14:32:13 +0300

    fix: make validation for empty array

--- src/services/statisticalTests.service.js
+++ src/services/statisticalTests.service.js
@@ -76,9 +76,9 @@ function validateData(sampleData) {
     throw new Error('Data is not an array.');
   }
 
-  const isValid = sampleData.every(
-    (item) => typeof item === 'number' && !isNaN(item)
-  );
+  const isValid =
+    sampleData.length > 0 &&
+    sampleData.every((item) => typeof item === 'number' && !isNaN(item));
 
   if (!isValid) {
     throw new Error('Invalid data: All elements must be numbers.');
